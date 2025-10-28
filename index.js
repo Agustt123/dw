c43d484c4bc510f3db878e88d1a0fdf52a9328f6
Agustt123 <agustintracheskyoficial@gmail.com>
2025-10-28 15:53:22 -0300

    dsadsad

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ const PORT = 13000;
 
 (async () => {
     try {
-        /*
         await actualizarEmpresas();
 
         // ⬇️⬇️⬇️ CAMBIO CLAVE: no await a la función con while(true)
@@ -69,7 +68,7 @@ const PORT = 13000;
                 running = false;
             }
         }, 1 * 60 * 1000);
-*/
+
         app.listen(PORT, () => {
             console.log(`Servidor escuchando en http://localhost:${PORT}`);
         });
