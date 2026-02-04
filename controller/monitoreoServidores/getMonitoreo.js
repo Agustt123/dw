63f15b08c2251ba0a9bcc21a2f4735d2a29d7338
Agustt123 <agustintracheskyoficial@gmail.com>
2026-02-04 14:36:13 -0300

    dsadad

---
+++ controller/monitoreoServidores/getMonitoreo.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const { executeQuery } = require("../../db");
+
+async function getMonitoreo(db) {
+    try {
+        const query = `SELECT * from monitoreo_servicios ORDER BY id DESC LIMIT 5`;
+        const results = await executeQuery(db, query);
+        return results;
+    } catch (error) {
+        throw new Error("Error al obtener el monitoreo de servicios: " + error.message);
+    }
+
+
+
+
+}
+
+
+await(db, sql, values);
+
+
+module.exports = { getMonitoreo };
--- route/monitoreo.js
+++ route/monitoreo.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const { getConnectionLocalCdc, getConnection } = require('../db');
 const { monitoreo } = require('../controller/monitoreoServidores/monitoreo');
+const { post, get } = require('./informe-colecta');
+const { getMonitoreo } = require('../controller/monitoreoServidores/getMonitoreo');
 
 const monitorear = express.Router();
 
@@ -19,6 +21,20 @@ monitorear.post('/', async (req, res) => {
         return res.status(200).json(resultado);
     } catch (error) {
         console.error("Error :", error);
+        return res.status(500).json({ estado: false, mensaje: "Error en el servidor" });
+    } finally {
+        if (db?.release) try { db.release(); } catch { }
+    }
+});
+
+
+monitorear.get('/', async (req, res) => {
+    const db = await getConnectionLocalCdc();
+    try {
+        const resultado = await getMonitoreo(db);
+        return res.status(200).json({ estado: true, data: resultado });
+    } catch (error) {
+        console.error("Error /testLocal:", error);
         return res.status(500).json({ estado: false, mensaje: "Error en el servidor" });
     } finally {
         if (db?.release) try { db.release(); } catch { }
